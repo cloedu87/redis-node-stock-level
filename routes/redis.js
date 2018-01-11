2b47c9eedb6e0abe85d8939c04bc9b17ed7481ba
Claudio S <cloedu@protonmail.ch>
2018-01-11 13:03:13 +0100

    implemented light weight soa pattern

--- routes/redis.js
+++ routes/redis.js
@@ -1,90 +1,76 @@
 const express = require('express');
 const router = express.Router();
-const redis = require("redis");
-const Promise = require("bluebird");
-
-const redisConfig = {
-    host: "pub-redis-17872.eu-central-1-1.1.ec2.redislabs.com",
-    port: 17872,
-    db: "redis-nano"
-};
-
-Promise.promisifyAll(redis.RedisClient.prototype);
-Promise.promisifyAll(redis.Multi.prototype);
+const redisService = require("../services/redis-service");
 
 /* post redis api. hash key-jsobject */
 router.post('/hash/entry', function (req, res, next) {
-    let client = getRedisClient();
     let key = req.body.key;
     let value = req.body.value;
 
-    console.log(JSON.stringify(value));
-    client.hmset(key, value, function (err) {
-        if (err) throw err;
-        client.quit();
-        res.sendStatus(201);
+    redisService.createHashEntry(key, value, function (err) {
+        if (err) {
+            console.error(err);
+            res.sendStatus(400)
+        }
+        else
+            res.sendStatus(201);
     });
 });
 
 /* get redis api. hash key-jsobject */
 router.get('/hash/entry/:id', function (req, res, next) {
-    let client = getRedisClient();
     let key = req.params.id;
 
-    client.hgetall(key, function (err, reply) {
-        if (err) throw err;
-        client.quit();
-        res.send(reply);
-    });
+    redisService.getHashEntry(key, function (err, reply) {
+        if(err) {
+            console.error(err);
+            res.sendStatus(400);
+        }
+        else
+            res.send(reply);
+    })
 });
 
 /* post redis api. string key-value */
 router.post('/string/entry', function (req, res, next) {
-    let client = getRedisClient();
     let key = req.body.key;
     let value = req.body.value;
 
-    client.set(key, value, function (err) {
-        if (err) throw err;
-        client.quit();
-        res.sendStatus(201);
-    });
+    redisService.createStringEntry(key, value, function (err, reply)  {
+        if(err) {
+            console.error(err);
+            res.sendStatus(400);
+        }
+        else
+            res.sendStatus(201);
+    })
 });
 
 /* get redis api. string key-value */
 router.get('/string/entry/:id', function (req, res, next) {
-    let client = getRedisClient();
     let key = req.params.id;
 
-    client.get(key, function (err, reply) {
-        if (err) throw err;
-        client.quit();
-        res.send(reply);
+    redisService.getStringEntry(key, function (err, reply) {
+        if(err) {
+            console.error(err);
+            res.sendStatus(400);
+        }
+        else
+            res.send(reply);
     });
 });
 
 /* delete redis api. flush all data in redis db */
 router.delete('/delete', function (req, res, next) {
-    let client = getRedisClient();
 
-    client.flushdb(function (err, succeeded) {
-        if (err) throw err;
-        client.quit();
-        if (succeeded) res.sendStatus(200);
-        if (!succeeded) res.sendStatus(500);
+    redisService.flushDatabase(function (err, succeeded) {
+        if(err || !succeeded) {
+            console.error(err);
+            res.sendStatus(400);
+        }
+        else
+            res.sendStatus(200);
     });
 });
-
-function getRedisClient() {
-    let client = redis.createClient(redisConfig.port, redisConfig.host, {no_ready_check: true});
-
-    client.auth('redis-nano-1', function (err) {
-        if (err) throw err;
-    });
-    client.on("error", function (err) {
-        if (err) throw err;
-    });
-    return client;
-}
 
 module.exports = router;
---
+++ services/redis-service.js
@@ -0,0 +1,60 @@
+const redis = require("redis");
+
+const redisConfig = {
+    host: "pub-redis-17872.eu-central-1-1.1.ec2.redislabs.com",
+    port: 17872,
+    db: "redis-nano"
+};
+
+let service = {
+    getRedisClient: function () {
+        let client = redis.createClient(redisConfig.port, redisConfig.host, {no_ready_check: true});
+
+        client.auth('redis-nano-1', function (err) {
+            if (err) throw err;
+        });
+        client.on("error", function (err) {
+            if (err) throw err;
+        });
+        return client;
+    },
+    getStringEntry: function (key, callback) {
+        let client = service.getRedisClient();
+
+        client.get(key, function (err, reply) {
+            client.quit();
+            callback(err, reply);
+        })
+    },
+    createStringEntry: function (key, value, callback) {
+        let client = service.getRedisClient();
+
+        client.set(key, value, function (err) {
+            client.quit();
+            callback(err);
+        });
+    },
+    getHashEntry: function (key, callback) {
+        let client = service.getRedisClient();
+        client.hgetall(key, function (err, reply) {
+            client.quit();
+            callback(err, reply);
+        });
+    },
+    createHashEntry: function (key, value, callback) {
+        let client = service.getRedisClient();
+        client.hmset(key, value, function (err) {
+            client.quit();
+            callback(err);
+        });
+    },
+    flushDatabase: function (callback) {
+        let client = service.getRedisClient();
+        client.flushdb(function (err, succeeded) {
+            client.quit();
+            callback(err, succeeded);
+        });
+    }
+};
+
+module.exports = service;
